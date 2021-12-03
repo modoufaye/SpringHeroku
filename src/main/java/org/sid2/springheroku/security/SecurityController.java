1903339af20139580c5ba1d2da5be1abb951241a
Modou Faye <modoufaye1107gmail.com>
2021-12-03 17:21:15 +0000

    Configuration avec Postgres

--- src/main/java/org/sid2/springheroku/security/SecurityController.java
+++ src/main/java/org/sid2/springheroku/security/SecurityController.java
@@ -1,4 +1,19 @@
 package org.sid2.springheroku.security;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SecurityController {
+
+    @GetMapping(value = "login")
+    public String connect(){
+        return "/login";
+    }
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "/login";
+    }
+
 }
